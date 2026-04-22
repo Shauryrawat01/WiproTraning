59b88dfd25a05ead32335efb917c58fc525a1c31
Shaury rawat <135199279+Shauryrawat01@users.noreply.github.com>
2026-04-22 22:14:51 +0530

    Add files via upload

---
+++ basic program/forrange.py
@@ -0,0 +1,13 @@
+# num = int(input('Enter a number'))
+#
+# sum =0
+# for i in range(1, num+1):
+#     sum = sum + i**2
+# print('sum of sq of nat nos :' sum)
+
+
+num = int(input("Enter how many *"))
+
+for i in range(1, num+1):
+    print('*')
+    
